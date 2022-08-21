dca8413480f12f40819dff51a2f2dd3373ca93c7
TeoDar <nikyl0777@gmail.com>
2022-08-22 01:06:24 +0300

    MVP ёпта!

--- parse.py
+++ parse.py
@@ -1,23 +1,8 @@
 # (c) https://t.me/TeoDar
-
-
 import requests
 import json
 
-
-# test_account = 'https://solscan.io/account/Epwf6ZXSY55Kwov4bhZmnEhF4BDyqbTUWC7f1oNRCrjt'
-
-# Ввод аккаунта и задание API ссылок
-account = 'Epwf6ZXSY55Kwov4bhZmnEhF4BDyqbTUWC7f1oNRCrjt'
-url_get_all_tokens = f'https://api.solscan.io/account/tokens?address={account}'
-url_get_account = "https://api.solscan.io/account?address="
-
-def get_data(account):
-    # Получить по API все токены на аккаунте в виде json
-    response_all_tokens = requests.get(url_get_all_tokens)
-    tokens = json.loads(response_all_tokens.text)['data']
-    print('[INFO]: Токены получены')
-    #Макет данных
+def _get_sample_characters():
     items = {
         'Имя':[],
         'Продано':[],
@@ -26,8 +11,34 @@ def get_data(account):
         'Изображение':[],
         'Семейство':[],
         'Сезон':[],
-        'Тип токена':[]
     }
+    return items
+
+def _get_sample_weapons():
+    items = {
+        'Имя':[],
+        'Продано':[],
+        'Аренда':[],
+        'Ссылка':[],
+        'Изображение':[],
+        'Семейство':[],
+        'Сезон':[],
+    }
+    return items
+
+
+
+
+def get_data(account):
+    url_get_all_tokens = f'https://api.solscan.io/account/tokens?address={account}'
+    url_get_account = "https://api.solscan.io/account?address="
+    # Получить по API все токены на аккаунте в виде json
+    response_all_tokens = requests.get(url_get_all_tokens)
+    tokens = json.loads(response_all_tokens.text)['data']
+    print('[INFO]: Токены получены')
+    #Макет данных
+    characters = _get_sample_characters()
+    weapons = _get_sample_weapons()
 
     for id, data in enumerate(tokens):
         if 'tokenSymbol' in data:
@@ -40,17 +51,30 @@ def get_data(account):
                 response_token_metadata = requests.get(url_metadata)
                 metadata = json.loads(response_token_metadata.text)
                 # Словарь в который будут складываться данные по токенам
-                items['Продано'].append(''),
-                items['Аренда'].append(''),
-                items['Ссылка'].append(f'https://solscan.io/token/{data["tokenAddress"]}'),
-                items['Изображение'].append(metadata.get('image')),
-                items['Имя'].append(metadata.get('name')),
-                items['Семейство'].append(metadata.get('collection').get('family')),
-                items['Сезон'].append(metadata.get('collection').get('name')),
-                items['Тип токена'].append(next((attr['value'] for attr in metadata.get('attributes') if attr['trait_type'] == 'Item Type'), None))
-                print(metadata.get('name'))
+                token_type = next((attr['value'] for attr in metadata.get('attributes') if attr['trait_type'] == 'Item Type'), None)
+                if token_type == 'Character':
+                    characters['Продано'].append(''),
+                    characters['Аренда'].append(''),
+                    characters['Ссылка'].append(f'https://solscan.io/token/{data["tokenAddress"]}'),
+                    characters['Изображение'].append(metadata.get('image')),
+                    characters['Имя'].append(metadata.get('name')),
+                    characters['Семейство'].append(metadata.get('collection').get('family')),
+                    characters['Сезон'].append(metadata.get('collection').get('name')),
+                    for attr in metadata.get('attributes'):
+                        characters[attr['trait_type']] = attr['value']
+                if token_type == 'Weapon':
+                    weapons['Продано'].append(''),
+                    weapons['Аренда'].append(''),
+                    weapons['Ссылка'].append(f'https://solscan.io/token/{data["tokenAddress"]}'),
+                    weapons['Изображение'].append(metadata.get('image')),
+                    weapons['Имя'].append(metadata.get('name')),
+                    weapons['Семейство'].append(metadata.get('collection').get('family')),
+                    weapons['Сезон'].append(metadata.get('collection').get('name')),
+                    for attr in metadata.get('attributes'):
+                        weapons[attr['trait_type']] = attr['value']
+                print(token_type, ':   ', metadata.get('name'))
                 if id > 10: break
-    return items
+    return characters, weapons
         
 if __name__=='__main__':
     get_data()
--- webapp.py
+++ webapp.py
@@ -1,30 +1,47 @@
 import streamlit as st
 import pandas as pd
-from parse import get_data
-
+from parse import get_data, _get_sample_characters, _get_sample_weapons
+ 
 
 class MiniRoyaleWebApp():
     def __init__(self) -> None:
-        pass
+        if 'characters' not in st.session_state:
+            st.session_state.characters = pd.DataFrame(_get_sample_characters())
+        if 'weapons' not in st.session_state:
+            st.session_state.weapons = pd.DataFrame(_get_sample_weapons())
+        if 'download' not in st.session_state:
+            st.session_state.download = self.right_column.download_button(label='Скачать', data='')
+            
     def window(self):
         st.write('Данный сайт предназначен для получения полной сводки всех имеющихся у вас токенов в MINI ROYALE.')
-        #Разделение виджетов на 2 колонки
-        left_column, right_column = st.columns(2)
         #Колонка с полем ввода
-        self.account = left_column.text_input(label='Введите токен вашего аккаунта на Solscan:')
+        self.account = st.text_input(label='Введите токен вашего аккаунта на Solscan:')
+        #Разделение виджетов на 2 колонки
+        self.left_column, self.right_column = st.columns(2)
         #Колонка с кнопками
-        with right_column:
-            self.btn_get_data = st.button('Выгрузить', on_click=self.get_tokens)
-            self.btn_download_data = st.button('Скачать CSV', disabled=True)
-
+        self.left_column.button('Выгрузить', on_click=self.get_tokens)
+        
+        st.write('Персонажи:')
+        st.write(st.session_state.characters)
+        st.write('Оружие:')
+        st.write(st.session_state.weapons)
+        
     def get_tokens(self):
         if self.account:
-            st.dataframe(pd.DataFrame(get_data(self.account)))
-        else:
-            pass
-        #self.btn_download_data.
-        
+            from pandas.io.excel import ExcelWriter
+            from io import BytesIO
+            characters, weapons = get_data(self.account)
+            st.session_state.characters = pd.DataFrame(characters)
+            st.session_state.weapons = pd.DataFrame(weapons)
+            output = BytesIO()
+            with ExcelWriter(output) as writer:
+                st.session_state.characters.to_excel(writer, index=False, sheet_name="Персонажи")
+                st.session_state.weapons.to_excel(writer, index=False, sheet_name="Оружие")
+            xls = output.getvalue()
+            st.session_state.download = self.right_column.download_button(label='Скачать',data=xls, file_name='tokens.xlsx', mime='text/xls')
 
 if __name__=='__main__':
     app = MiniRoyaleWebApp()
     app.window()
+    
+#ERoSDccar6zKd3boNboq2Y65nx4u8yRGRc5pbYcsMfSt
